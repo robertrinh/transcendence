fd88947f4577f236df52e3d13f19f2a4efda6e4c
Rutger Meuzelaar <rmeuzela@student.codam.nl>
2025-08-28 16:27:11 +0200

    Add example from fastify docs for testing purposes

---
+++ src/server.js
@@ -0,0 +1,18 @@
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+  return { hello: 'world' }
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: 8080, host: '0.0.0.0' })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
