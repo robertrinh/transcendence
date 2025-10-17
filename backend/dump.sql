60cf9e45a84ff2d252383ae1b0110daeee0d45ee
robertrinh <robert.trinh@yahoo.com>
2025-10-17 18:39:35 +0200

    added test users to the database

--- backend/dump.sql
+++ backend/dump.sql
@@ -31,4 +31,12 @@ CREATE TABLE IF NOT EXISTS tournaments (
     round_type TEXT,
     FOREIGN KEY (game_id) REFERENCES games(id)
 );
+
+-- I need some test users: delete afterwards :)
+INSERT INTO users (username, password) VALUES 
+    ('robert', 'hatesSQL'),
+    ('rutger', 'thenotvimzelaar1'),
+    ('ellen', 'ourSPAlegend'),
+	('joao', 'designking');
+
 COMMIT;
