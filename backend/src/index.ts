393b89af55ce807d0fb80a9920bd6da3d127867a
robertrinh <robert.trinh@yahoo.com>
2025-10-17 18:41:49 +0200

    registered CORS + Routes to API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,33 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import databaseRoutes from './database.js'
+import usersRoutes from './routes/users.js'
 
 const fastify = Fastify({
   logger: true
 })
 
+//* CORS (to allow frontend to access backend)
+await fastify.register(cors, {
+	origin: 'http://localhost:8080',
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	allowedHeaders: ['Content-Type', 'Authorization'],
+	credentials: true,
+})
+
+//* Database utility routes (e.g. testing/debugging)
 fastify.register(
   databaseRoutes, {
     prefix: "/api/db"
 })
 
-// API Health check
+//* User logic routes
+fastify.register(
+  usersRoutes, {
+    prefix: "/api"
+})
+
+//* API Health check
 fastify.get('/api/health', async (request, reply) => {
   return { status: 'OK', message: 'Backend API is running', database: 'SQLite3 is connected' }
 })
