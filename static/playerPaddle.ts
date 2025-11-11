3528225433ca4991276701a81086263df83c45ec
Rutger Meuzelaar <rmeuzela@student.codam.nl>
2025-11-11 17:17:13 +0100

    Cherry-pick client side Pong code from commit 44214c3

---
+++ static/playerPaddle.ts
@@ -0,0 +1,72 @@
+export class PlayerPaddle
+{
+    x: number
+    y: number
+    width: number
+    height: number
+    yVector: number
+    color: string
+    upPressed = false
+    downPressed = false
+
+    constructor(x: number, y: number, width: number, height: number, yVector: number, color: string) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.yVector = yVector
+        this.color = color
+    }
+
+    private hasVerticalCollision(canvas: HTMLCanvasElement, newY: number): boolean {
+        if (
+            newY < 0 ||
+            newY + this.height > canvas.height
+        ) {
+            return true
+        }
+        return false
+    }
+
+    moveUp(canvas: HTMLCanvasElement, deltaTimeSeconds: number) {
+        const newY = this.y - this.yVector * deltaTimeSeconds
+
+        if (this.hasVerticalCollision(canvas, newY)) {
+            return
+        }
+        this.y = newY
+    }
+
+    moveDown(canvas: HTMLCanvasElement, deltaTimeSeconds: number) {
+        const newY = this.y + this.yVector * deltaTimeSeconds
+
+        if (this.hasVerticalCollision(canvas, newY)) {
+            return
+        }
+        this.y = newY
+    }
+
+    draw(canvas: HTMLCanvasElement, canvasCtx: CanvasRenderingContext2D, deltaTimeSeconds: number) {
+        canvasCtx.fillStyle = this.color
+        canvasCtx.fillRect(this.x, this.y, this.width, this.height)
+        canvasCtx.fillStyle = "#ffffff"
+        if (this.downPressed) {
+            this.moveDown(canvas, deltaTimeSeconds)
+        }
+        if (this.upPressed) {
+            this.moveUp(canvas, deltaTimeSeconds)
+        }
+    }
+}
+
+export namespace player_paddle
+{
+    export enum hitSide
+    {
+        Top,
+        Bottom,
+        Left,
+        Right,
+        None
+    } 
+}
