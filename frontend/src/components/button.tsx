d6b140977b8d9073ae5759d9da87f9790b22cd90
Rutger Meuzelaar <rmeuzela@student.codam.nl>
2025-11-19 23:37:31 +0100

    Add id attribute

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 interface Button {
+    id: string
     className: string
     buttonName: string
     onClick: () => void
 }
 
-export default function Button({className, buttonName, onClick}:Button) {
+export default function Button({id, className, buttonName, onClick}:Button) {
     return (
-        <button className={className} onClick={onClick}>
+        <button id={id} className={className} onClick={onClick}>
             {buttonName}
         </button>
     )
