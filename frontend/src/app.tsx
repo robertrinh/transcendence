334634f96d616374f753657c1e6b616b7851dd72
Rutger Meuzelaar <rmeuzela@student.codam.nl>
2025-12-05 17:02:51 +0100

    Remove unused import

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import MainLayout from './components/Layout/MainLayout';
 
 // Import your views
@@ -110,4 +110,5 @@ export function App() {
             </MainLayout>
         </div>
     );
+
 }
