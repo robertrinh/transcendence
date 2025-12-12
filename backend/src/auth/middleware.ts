848111cb1dd2e3460ec193ef41414d083123632a
robertrinh <robert.trinh@yahoo.com>
2025-12-12 16:08:22 +0100

    added middleware functionality to authenthicate routes that needs security

---
+++ backend/src/auth/middleware.ts
@@ -0,0 +1,37 @@
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { verifyToken, TokenPayload} from './utils.js';
+
+declare module 'fastify' {
+	interface FastifyRequest {
+		user?: TokenPayload;
+	}
+}
+
+//* When to use middleware:
+//* 1. When you need to verify a JWT token
+//* 2. When you need to check if a user is authenticated (login)
+//* 3. When you need to check if a user is authorized to access a resource (e.g. users routes)
+//* 4. When you need to check if a user is authenticated and authorized to access a resource (e.g. admin only, no user)
+//* example: as a user, you can only access your own data (like my stats, profile, etc.)
+//* as admin, you can access all data (like all users, all games, all tournaments, etc.)
+//* adding middleware helps protect routes from unauthorized access!
+
+//* Middleware to verify JWT token for authenticated routes
+export const authenticate = async (request: FastifyRequest, reply: FastifyReply) => {
+	const authHeader = request.headers.authorization;
+	if (typeof authHeader !== 'string' || !authHeader.startsWith('Bearer ')) {
+		return reply.code(401).send({
+			success: false,
+			error: 'No token provided bruv'
+		})
+	}
+	const token = authHeader.split(' ')[1];
+	const payload = verifyToken(token);
+	if (!payload) {
+		return reply.code(401).send({
+			success: false,
+			error: 'Invalid or expired token brud'
+		})
+	}
+	request.user = payload;
+}
