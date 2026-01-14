0e9aeff464b2f9c87504de00f80bbee2ed337e7b
Rutger Meuzelaar <rmeuzela@student.codam.nl>
2026-01-14 17:32:10 +0100

    Implement server reconciliation for player movement

--- frontend/src/static/gameOnline.ts
+++ frontend/src/static/gameOnline.ts
@@ -1,16 +1,60 @@
 import { Ball } from './ball.js'
+import { PlayerPaddle } from './playerPaddle.js'
 
 export async function gameOnlineLobby(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, socket: WebSocket) {
+    const p1Color = "#5885A2"
+    const p2Color = "#B8383B"
+
     function handleKeyDown(key: KeyboardEvent) {
         switch (key.key) {
             case "ArrowDown":
-                socket.send(JSON.stringify({type: "MOVE_DOWN"}))
+                playerOne.downPressed = true
                 break
             case "ArrowUp":
-                socket.send(JSON.stringify({type: "MOVE_UP"}))
+                playerOne.upPressed = true
+                break
+            case "s":
+                playerOne.downPressed = true
+                break
+            case "w":
+                playerOne.upPressed = true
                 break
         }
         key.preventDefault()
+    }
+
+    function handleKeyUp(key: KeyboardEvent) {
+        switch (key.key) {
+            case "ArrowDown":
+                playerOne.downPressed = false
+                break
+            case "ArrowUp":
+                playerOne.upPressed = false
+                break
+            case "s":
+                playerOne.downPressed = false
+                break
+            case "w":
+                playerOne.upPressed = false
+                break
+        }
+        key.preventDefault()
+    }
+
+    function processMovement(deltaTimeSeconds: number) {
+        const timestamp = +new Date()
+        if (playerOne.downPressed) {
+            const moveObj = {type: "MOVE_DOWN", ts: timestamp}
+            playerOne.moveDown(canvas, deltaTimeSeconds)
+            gameSocket.send(JSON.stringify(moveObj))
+            pendingMoves.push(moveObj)
+        }
+        if (playerOne.upPressed) {
+            const moveObj = {type: "MOVE_UP", ts: timestamp}
+            playerOne.moveUp(canvas, deltaTimeSeconds)
+            gameSocket.send(JSON.stringify(moveObj))
+            pendingMoves.push(moveObj)
+        }
     }
 
     class Point {
@@ -54,26 +98,82 @@ export async function gameOnlineLobby(canvas: HTMLCanvasElement, ctx: CanvasRend
         now = newTime
         deltaTimeMS = now - then
         if (deltaTimeMS > fpsInterval) {
+            processMovement(1 + (deltaTimeMS / 1000))
             then = now - (deltaTimeMS % fpsInterval)
             ctx.clearRect(0, 0, canvas.width, canvas.height)
-            ball.draw(ctx)
             moveBall(canvas, ball)
+            ball.draw(ctx)
+            playerOne.draw(ctx)
+            playerTwo.draw(ctx)
         }
     }
 
     let interpVelocity: Point
+    let pendingMoves = new Array<MoveTS>()
+
+    interface MoveTS {
+        type: string,
+        timestamp: number
+    }
+
+    function updatePendingMoves(lastServerTime: number) {
+        let i = 0
+        if (pendingMoves.length === 0) {
+            return
+        }
+        while (i < pendingMoves.length) {
+            const movePair = pendingMoves[i]
+            if (movePair.timestamp >= lastServerTime) {
+                break
+            }
+            if (movePair.type === 'MOVE_UP') {
+                playerOne.moveUp(canvas, 1)
+            }
+            else if (movePair.type === 'MOVE_DOWN') {
+                playerOne.moveDown(canvas, 1)
+            }
+            i++
+        }
+        pendingMoves.splice(0, i)
+    }
+
+    let playerID = 0 // playerID given by server
 
     function gameSockOnMessage(event: MessageEvent) {
         const JSONObject = JSON.parse(event.data)
-        console.log(JSONObject.type)
+        const p1Server = JSONObject.p1
+        const p2Server = JSONObject.p2
+        const ballServer = JSONObject.ball
         switch (JSONObject.type) {
             case "STATE":
-                interpVelocity = pointSubtract(new Point(JSONObject.ball.x, JSONObject.ball.y), new Point(ball.x, ball.y))
+                interpVelocity = pointSubtract(new Point(ballServer.x, ballServer.y), new Point(ball.x, ball.y))
                 interpVelocity.x /= serverTick
                 interpVelocity.y /= serverTick
+                if (playerID === 1) {
+                    playerOne.x = p1Server.x
+                    playerOne.y = p1Server.y
+                    updatePendingMoves(p1Server.last_ts)
+                }
+                else {
+                    playerOne.x = p2Server.x
+                    playerOne.y = p2Server.y
+                    updatePendingMoves(p2Server.last_ts)
+                }
                 break
             case "LOBBY_WAIT":
                 console.log("Waiting for other players to connect...")
+                break
+            case "ID":
+                playerID = JSONObject.player_id as number
+                // move paddle to the right side
+                if (playerID === 2) {
+                    playerOne.x = canvas.width-70
+                    playerOne.y = 50
+                    playerTwo.x = 40
+                    playerTwo.y = 50
+                }
+                console.log(`You are player ${playerID}`)
+                gameSocket.send(JSON.stringify({type: 'READY'}))
                 break
             default:
                 console.log(`Unrecognized message type: ${JSONObject.type}`)
@@ -81,7 +181,7 @@ export async function gameOnlineLobby(canvas: HTMLCanvasElement, ctx: CanvasRend
     }
 
     function gameSockOnOpen() {
-        gameSocket.send(JSON.stringify({type: 'READY'}))
+        gameSocket.send(JSON.stringify({type: 'WHOAMI'}))
     }
 
     let gameSocket: WebSocket
@@ -103,6 +203,10 @@ export async function gameOnlineLobby(canvas: HTMLCanvasElement, ctx: CanvasRend
     }
     
     canvas.addEventListener("keydown", handleKeyDown)
+    canvas.addEventListener("keyup", handleKeyUp)
+    const paddleMoveUnits = 30
     const ball = new Ball(canvas.width / 2, canvas.height / 2, {x: 1, y: 1}, 15, 2, "#a31621", 0, 7.5)
+    const playerOne = new PlayerPaddle(40, 50, 20, 150, paddleMoveUnits, p1Color)
+    const playerTwo = new PlayerPaddle(canvas.width - 70, 50, 20, 150, paddleMoveUnits, p2Color) 
     startAnimate(60)
 }
--- server-side-pong/game.py
+++ server-side-pong/game.py
@@ -8,18 +8,25 @@ import json
 import random as rand
 from ball import Ball, Vector2
 from time import time
+from player_paddle import PlayerPaddle
 
 # game dimensions
 ARENA_WIDTH = 1024
 ARENA_HEIGHT = 768
 
-PLAYERS = set()
+PLAYERS = list()
 READY_COUNT = 0
 TICK = 1000 / 10
 screen = None
-
-BALL_SPEED_UNITS_S = 1000  # The ball speed in units per second
 BALL_RADIUS = 15
+
+# movement
+BALL_SPEED_UNITS_S = 500  # The ball speed in units per second
+PADDLE_SPEED_UNITS_S = 30
+P1_INPUT = list()
+P2_INPUT = list()
+P1_LAST_TS = None
+P2_LAST_TS = None
 
 
 def in_hor_arena_bounds(y: int, radius_px: int) -> bool:
@@ -30,7 +37,6 @@ def in_vert_arena_bounds(x: int, radius_px: int) -> bool:
     return x >= 0 and x <= ARENA_WIDTH - radius_px * 2
 
 
-# simulate a bouncing ball first
 def move_ball(ball: Ball):
     new_pos = Vector2(
         ball.shape.x + (ball.dir_vect.x * ball.movement_speed),
@@ -53,32 +59,41 @@ def random_ball_vec() -> Vector2:
     return Vector2(x, y)
 
 
-# player_one = PlayerPaddle(1.005, pygame.Rect(50, 50, 20, 150))
-# player_two = PlayerPaddle(1.005, pygame.Rect(ARENA_WIDTH-70, 50, 20, 150))
+def update(ball: Ball, player_one: PlayerPaddle, player_two: PlayerPaddle):
+    move_ball(ball)
+    process_input(player_one, player_two)
 
 
-def draw(ball: Ball):
+def draw(ball: Ball, player_one: PlayerPaddle, player_two: PlayerPaddle):
     screen.fill([255, 255, 255])
-    # pygame.draw.rect(screen, (200, 35, 50), player_one.shape, 2)
-    # pygame.draw.rect(screen, (200, 35, 50), player_two.shape, 2)
     ball.draw(screen)
+    player_one.draw(screen)
+    player_two.draw(screen)
     pygame.display.update()
 
 
-def form_game_state(ball: Ball):
+def game_loop(
+        ball: Ball, player_one: PlayerPaddle,
+        player_two: PlayerPaddle):
     game_state = {
         'type': 'STATE',
-        'last_ts': time() * 1000,
         'ball': {
             'x': ball.shape.x,
             'y': ball.shape.y,
-            'dir_x': ball.dir_vect.x,
-            'dir_y': ball.dir_vect.y,
-            'speed': BALL_SPEED_UNITS_S,
-            }
+            },
+        'p1': {
+            'x': player_one.shape.x,
+            'y': player_one.shape.y,
+            'last_ts': P1_LAST_TS
+        },
+        'p2': {
+            'x': player_two.shape.x,
+            'y': player_two.shape.y,
+            'last_ts': P2_LAST_TS
         }
-    move_ball(ball)
-    draw(ball)
+    }
+    update(ball, player_one, player_two)
+    draw(ball, player_one, player_two)
     terminate = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -96,26 +111,70 @@ def increment_ready():
     READY_COUNT += 1
 
 
+def process_paddle(input: list, paddle: PlayerPaddle):
+    for move in input:
+        if move[0] == 'DOWN':
+            paddle.move_down(ARENA_HEIGHT)
+        elif move[0] == 'UP':
+            paddle.move_up()
+
+
+def process_input(player_one: PlayerPaddle, player_two: PlayerPaddle):
+    global P1_LAST_TS
+    global P2_LAST_TS
+    if len(P1_INPUT) != 0:
+        process_paddle(P1_INPUT, player_one)
+        P1_LAST_TS = P1_INPUT[-1][1]
+        P1_INPUT.clear()
+    if len(P2_INPUT) != 0:
+        process_paddle(P2_INPUT, player_two)
+        P2_LAST_TS = P2_INPUT[-1][1]
+        P2_INPUT.clear()
+
+
 async def handler(websocket: websockets.ServerConnection):
     # some authentication here?
-    PLAYERS.add(websocket)
+    PLAYERS.append(websocket)
     print(websocket)
     async for message in websocket:
         try:
             message_content = json.loads(message)
         except json.decoder.JSONDecodeError as e:
             print(e)
-            continue
+            exit(1)
         message_type = message_content['type']
         match message_type:
             case 'READY':
                 increment_ready()
                 print(websocket, " is ready")
                 if READY_COUNT < 2:
-                    asyncio.create_task(game_loop())
+                    asyncio.create_task(game_lobby())
+            case 'MOVE_DOWN':
+                if websocket == PLAYERS[0]:
+                    print("Player 1 wants to move down")
+                    P1_INPUT.append(['DOWN', message_content['ts']])
+                else:
+                    print("Player 2 wants to move down")
+                    P2_INPUT.append(['DOWN', message_content['ts']])
+            case 'MOVE_UP':
+                if websocket == PLAYERS[0]:
+                    print("Player 1 wants to move up")
+                    P1_INPUT.append(['UP', message_content['ts']])
+                else:
+                    print("Player 2 wants to move up")
+                    P2_INPUT.append(['UP', message_content['ts']])
+            case 'WHOAMI':
+                if websocket == PLAYERS[0]:
+                    await websocket.send(json.dumps(
+                        {'type': 'ID', 'player_id': 1}
+                        ))
+                else:
+                    await websocket.send(json.dumps(
+                        {'type': 'ID', 'player_id': 2}
+                        ))
 
 
-async def game_loop():
+async def game_lobby():
     while READY_COUNT != 2:
         message = {'type': 'LOBBY_WAIT'}
         broadcast(PLAYERS, json.dumps(message))
@@ -126,8 +185,18 @@ async def game_loop():
         ARENA_HEIGHT / 2,
         random_ball_vec(),
         conv_ball_speed, conv_ball_speed * 2, BALL_RADIUS)
+    player_one = PlayerPaddle(
+        PADDLE_SPEED_UNITS_S,
+        pygame.Rect(50, 50, 20, 150),
+        pygame.Color(55, 100, 40)
+        )
+    player_two = PlayerPaddle(
+        PADDLE_SPEED_UNITS_S,
+        pygame.Rect(ARENA_WIDTH-70, 50, 20, 150),
+        pygame.Color(55, 100, 40)
+        )
     while True:
-        broadcast(PLAYERS, json.dumps(form_game_state(ball)))
+        broadcast(PLAYERS, json.dumps(game_loop(ball, player_one, player_two)))
         await asyncio.sleep(TICK/1000)
 
 
