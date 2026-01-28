1ea49c10913712665acf9ecdeda15370163e173d
Quoc Quan Robert Trinh <qtrinh@f1r3s2.codam.nl>
2026-01-28 15:06:00 +0100

    reverted api/health back to monke, pls pass test

--- backend/src/routes/generic.ts
+++ backend/src/routes/generic.ts
@@ -1,35 +1,12 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify';
-// import { sseConnections, chatMessages } from '../index.js';
+
 
 export default async function genericRoutes (
 	fastify: FastifyInstance,
 	options: FastifyPluginOptions
 ) {
     // Health check
-    fastify.get('/health', async (request, reply) => {
-        return { 
-            status: 'OK', 
-            timestamp: new Date().toISOString(),
-            backend: 'running',
-            database: 'connected',
-            // activeConnections: sseConnections.size,
-            // totalMessages: chatMessages.length
-        };
+    fastify.get('/api/health', (request, reply) => {
+        reply.status(200).send({ message: 'API is healthy' });
     });
 }
-//     // Root endpoint
-//     fastify.get('/', async (request, reply) => {
-//         return { 
-//             message: 'ft_transcendence Backend API',
-//             status: 'running',
-//             endpoints: {
-//                 health: '/api/health',
-//                 auth: '/api/auth/*',
-//                 chat: '/api/chat/*',
-//                 database: '/api/db/*',
-//                 chatStream: '/api/chat/stream (SSE)'
-//             }
-//         };
-//     });
-// }
-
