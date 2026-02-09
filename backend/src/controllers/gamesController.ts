554e396bd8ffe26bc8930cb96b24d0ab1d7824e3
ekenln <eklund.ellen.ee@gmail.com>
2026-02-09 23:15:07 +0100

    added new endpoints for matchmaking, work in progress

--- backend/src/controllers/gamesController.ts
+++ backend/src/controllers/gamesController.ts
@@ -1,6 +1,9 @@
 import { ApiError } from '../Errors/errors.js';
 import { gamesService } from '../services/gamesService.js'
 import { FastifyRequest, FastifyReply } from 'fastify';
+import { userService } from '../services/userService.js';
+import { Player } from '../types/database.interfaces.js'
+
 
 export const gamesController = {
 
@@ -9,20 +12,82 @@ export const gamesController = {
 		return {success: true, data: games }
     },
 
-	createGame: async (req: FastifyRequest, reply: FastifyReply) => {
-		const { player1_id, player2_id } = req.body as { player1_id: number , player2_id: number }
-		if (player1_id === player2_id)
-			throw new ApiError(400, 'must be two different users');
-		const game = gamesService.addGame(player1_id, player2_id);
-		return {success: true, game, message: 'Game created'};
-	},
-
 	getGameByID: async (req: FastifyRequest, reply: FastifyReply) => {
 		const { id } = req.params as { id: number }
 		const game = gamesService.fetchGame(id);
 		if (!game)
 			throw new ApiError(404, 'Game not found');
 		return { success: true, data: game }
+	},
+
+	getGameQueue: async () => {
+		const queue = gamesService.fetchGameQueue();
+		return {success: true, data: queue }
+    },
+
+	matchMaking: async (req: FastifyRequest, reply: FastifyReply) => {
+		const player_id = req.user!.userId;
+
+		const player = userService.fetchUser(player_id) as Player;
+		if (player.status === 'playing' || player.status === 'searching')
+			throw new ApiError(400, "player already playing");
+
+		const waitingPlayer = gamesService.findWaitingPlayer(player_id);
+
+		if (waitingPlayer) {
+			const game = gamesService.createGame(waitingPlayer.player_id, player_id);
+			return {success: true, data: game, message: 'Game created, connect to gameserver'}
+		}
+		else {
+			gamesService.addtoGameQueue(player_id);
+			return {success: true, message: 'Player added to queue, waiting for other player to join'}
+		}
+	},
+
+	getMatchmakingStatus: async (req: FastifyRequest, reply: FastifyReply) => {
+		const player_id = req.user!.userId;
+		const status = gamesService.matchmakingStatus(player_id);
+		return {success: true, data: status }
+    },
+
+	cancelMatchmaking: async (req: FastifyRequest, reply: FastifyReply) => {
+		const result = gamesService.cancelMatchmaking(req.user!.userId);
+		return {success: true, message: 'player removed from game queue'}
+	},
+
+	hostLobby: async (req: FastifyRequest, reply: FastifyReply) => {
+		const player_id = req.user!.userId;
+		const player = userService.fetchUser(player_id) as Player;
+		if (player.status === 'playing' || player.status === 'searching')
+			throw new ApiError(400, "player already playing");
+		const queue = gamesService.hostLobby(player_id);
+		return {success: true, data: queue, message: 'Player created private game'}
+	},
+
+	joinLobby: async (req: FastifyRequest, reply: FastifyReply) => {
+		const player_id = req.user!.userId;
+		const { lobby_id } = req.body as { lobby_id: string }
+
+		const player = userService.fetchUser(player_id) as Player;
+		if (player.status === 'playing' || player.status === 'searching')
+			throw new ApiError(400, "player already playing");
+
+		const game_queue = gamesService.fetchlobby(lobby_id);
+		if (game_queue === undefined)
+			throw new ApiError(404, 'lobby not found')
+
+		const game = gamesService.createGame(game_queue.player_id, player_id);
+		return {success: true, data: game, message: 'Game created, connect to gameserver'}
+	},
+
+	finishGame: async (req: FastifyRequest, reply: FastifyReply) => {
+		const { id } = req.params as { id: number }
+		const {winner_id, score_player1, score_player2, finished_at } = req.body as {winner_id: number, score_player1: number, score_player2: number, finished_at: number };
+		const result = gamesService.finishGame(id, score_player1, score_player2, winner_id, finished_at);
+		return {
+			success: true,
+			message: 'Game finished'
+		}
 	},
 	
 	deleteGame: async (req: FastifyRequest, reply: FastifyReply) => {
@@ -34,27 +99,9 @@ export const gamesController = {
 			success: true, 
 			message: 'Game deleted' 
 		}
-	},
+	}
 
 	//What happens if a player exits mid game? 
 	//What are errors that can happen?
-	updateGame: async (req: FastifyRequest, reply: FastifyReply) => {
-		const { id } = req.params as { id: number }
-		const {score_player1, score_player2 } = req.body as { score_player1: number, score_player2: number };
-		const result = gamesService.updateGame(id, score_player1, score_player2);
-		return {
-			success: true,
-			message: 'Game updated'
-		}
-	},
-
-	finishGame: async (req: FastifyRequest, reply: FastifyReply) => {
-		const { id } = req.params as { id: number }
-		const {winner_id, score_player1, score_player2, finished_at } = req.body as {winner_id: number, score_player1: number, score_player2: number, finished_at: number };
-		const result = gamesService.finishGame(id, score_player1, score_player2, winner_id, finished_at);
-		return {
-			success: true,
-			message: 'Game finished'
-		}
-	}
-	}
+	//need to add some clean up funcs if something is left opened or so
+}
--- backend/src/services/gamesService.ts
+++ backend/src/services/gamesService.ts
@@ -1,11 +1,32 @@
 import { db } from '../databaseInit.js'
 import { dbError } from '../Errors/dbErrors.js'
+import { ApiError } from '../Errors/errors.js';
+import { Player, Queue, Game } from '../types/database.interfaces.js'
 
+const TIMEOUT_MATCHMAKING = 30000 //in millisec
 
 export const gamesService = {
 
 	getAllGames: () => {
 		return db.prepare('SELECT * FROM games').all();
+	},
+
+	fetchGame: (id: number | bigint) =>{
+		return db.prepare('SELECT * FROM games WHERE id = ?').get(id);
+	},
+
+	addtoGameQueue: (player: number) => {
+		const result = db.prepare('INSERT INTO game_queue (player_id) VALUES (?)').run(player);
+		db.prepare('UPDATE users SET status = ? WHERE id = ?').run('searching', player);
+		return result;
+	},
+
+	fetchGameQueue: () =>{
+		return db.prepare('SELECT * FROM game_queue').all() as Queue[];
+	},
+
+	findWaitingPlayer: (player_id: number) => {
+		return db.prepare('SELECT player_id, joined_at FROM game_queue WHERE player_id != ? AND private = 0 ORDER BY joined_at ASC LIMIT 1 ').get(player_id) as Queue | undefined;
 	},
 
 	addGame: (player1_id: number, player2_id: number) => {
@@ -17,19 +38,76 @@ export const gamesService = {
 		}
 	},
 
-	fetchGame: (id: number) =>{
-		return db.prepare('SELECT * FROM games WHERE id = ?').get(id);
+	createGame: (player: number, new_player: number) => {
+		if (player === new_player)
+			throw new ApiError(400, "duplicate player");
+		try {
+			const game_created = db.prepare('INSERT INTO games (player1_id, player2_id, status) VALUES(?, ?, ?) RETURNING *').get(player, new_player, 'ready') as Game;
+			db.prepare('UPDATE users SET status = ? WHERE id = ? OR id = ?').run('matched', player, new_player);
+			db.prepare('DELETE FROM game_queue WHERE player_id = ?').run(player);
+			return game_created;
+		}
+		catch (err: any) {
+			dbError(err);
+		}	
+	},
+
+	matchmakingStatus: (player_id: number) => {
+		const player = db.prepare('SELECT status FROM users WHERE id = ?').get(player_id) as Player;
+		if (player.status === 'matched') {
+			db.prepare('UPDATE users SET status = ? WHERE id = ?').run('playing', player_id);
+			return db.prepare('SELECT * FROM games WHERE player1_id = ? OR player2_id = ? ORDER BY created_at DESC LIMIT 1').get(player_id, player_id); //return gamedata
+		}
+		if (player.status === 'searching') {
+			const queue = db.prepare('SELECT joined_at FROM game_queue WHERE player_id = ?').get(player_id) as Queue;
+			if (Date.now() - queue.joined_at * 1000 > TIMEOUT_MATCHMAKING) {
+				db.prepare('UPDATE users SET status = ? WHERE id = ?').run('idle', player_id); //timed out!!!!
+				db.prepare('DELETE FROM game_queue WHERE player_id = ?').run(player_id);
+				return db.prepare('SELECT status FROM users WHERE id = ?').get(player_id) as Player;
+			}
+		}
+		return player;
+	},
+
+	cancelMatchmaking: (player_id: number) => {
+		db.prepare('UPDATE users SET status = ? WHERE id = ?').run('idle', player_id)
+		return db.prepare('DELETE FROM game_queue WHERE player_id = ?').run(player_id)
+	},
+
+	hostLobby: (player_id: number) => {
+		const lobby_id = generateLobbyId();
+		const queue = db.prepare('INSERT INTO game_queue (player_id, private, lobby_id) VALUES(?, ?, ?) RETURNING *').get(player_id, 1, lobby_id) as Queue;
+		db.prepare('UPDATE users SET status = ? WHERE id = ?').run('searching', player_id)
+		return queue;
+	},
+
+	fetchlobby: (lobby_id: string ) => {
+		return db.prepare('SELECT * FROM game_queue WHERE lobby_id = ?').get(lobby_id) as Queue;
+	},
+
+	finishGame: (id:number, score_player1:number, score_player2:number, winner_id: number, finished_at: number) =>{
+		const gameObj = gamesService.fetchGame(id) as Game;
+		if (gameObj.status !== 'ready') //check this
+			throw new ApiError(400, 'game not ongoing');
+		try {
+			db.prepare('UPDATE users SET status = ? WHERE id = ? OR id = ?').run('finished', gameObj.player1_id, gameObj.player2_id);
+			return db.prepare(' UPDATE games SET winner_id = ?, score_player1 = ?, score_player2 = ?, finished_at = ?, status = ? WHERE id = ?').run(winner_id, score_player1, score_player2, finished_at, 'finished', id)
+		}
+		catch (err:any) {
+			dbError(err);
+		}
 	},
 
 	removeGame: (id: number) => {
 		return db.prepare('DELETE FROM games WHERE id = ?').run(id)
-	},
-
-	updateGame: (id:number, score_player1:number, score_player2:number) => {
-		return db.prepare(' UPDATE games SET score_player1 = ?, score_player2 = ? WHERE id = ?').run(score_player1, score_player2, id)
-	},
-
-	finishGame: (id:number, score_player1:number, score_player2:number, winner_id: number, finished_at: number) =>{
-		return db.prepare(' UPDATE games SET winner_id = ?, score_player1 = ?, score_player2 = ?, finished_at = ?, status = ? WHERE id = ?').run(winner_id, score_player1, score_player2, finished_at, 'finished', id)
 	}
+}
+
+function generateLobbyId(): string {
+	const hex = '0123456789abcdef';
+	let output = '';
+	for (let i = 0; i < 10; ++i) {
+		output += hex.charAt(Math.floor(Math.random() * hex.length));
+	}
+	return output;
 }
