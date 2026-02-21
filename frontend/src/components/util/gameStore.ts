d80d6b2b6609c9ce51d7f19f96d15e8fd1665fae
ekenln <eklund.ellen.ee@gmail.com>
2026-02-21 16:31:44 +0100

    added the gameStore

---
+++ frontend/src/components/util/gameStore.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand'
+import { Screen, GameMode } from '../game/types.ts'
+
+type GameStore = {
+    screen: Screen
+    gameMode: GameMode
+
+    setScreen: (screen: Screen) => void
+    setGameMode: (gameMode: GameMode) => void
+}
+
+const useGameStore = create<GameStore>((set) => ({
+    screen: 'main',
+    gameMode: 'none',
+
+
+    setScreen: (screen: Screen) => set({ screen }),
+    setGameMode: (mode: GameMode) => set({ gameMode: mode })
+}))
+
+export default useGameStore;
