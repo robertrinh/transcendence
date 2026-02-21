2868d50a4f165aab71ba7d0c6e9b8ff641d2a156
ekenln <eklund.ellen.ee@gmail.com>
2026-02-21 13:54:39 +0100

    added back the 'matched' state

--- backend/dump.sql
+++ backend/dump.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS users (
     created_at DATETIME DEFAULT (datetime('now')),
     avatar_id INTEGER,
     email TEXT,
-    status TEXT CHECK (status IN ('idle', 'searching', 'playing')) DEFAULT 'idle',
+    status TEXT CHECK (status IN ('idle', 'searching', 'matched', 'playing')) DEFAULT 'idle',
     last_login DATETIME,
     two_factor_secret TEXT,              -- 2FA TOTP secret key
     two_factor_enabled BOOLEAN DEFAULT 0, -- to check if 2FA is enabled
