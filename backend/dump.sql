6f4bde6478bffa9144a57362ee708d672f5e21ed
ekenln <eklund.ellen.ee@gmail.com>
2026-03-03 15:32:53 +0100

    added back id on tournamnet_participants table, probably not needed but is being fetched

--- backend/dump.sql
+++ backend/dump.sql
@@ -117,6 +117,7 @@ CREATE TABLE IF NOT EXISTS blocked (
 );
 
 CREATE TABLE IF NOT EXISTS tournament_participants (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	tournament_id INTEGER NOT NULL,
 	user_id INTEGER NOT NULL,
 	user_left BOOLEAN CHECK(user_left IN (0,1)) DEFAULT 0 NOT NULL,
